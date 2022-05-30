6ed6cc90365bac4f6175842c8bf0834c8e4bf7e1
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-05-31 00:17:53 +0530

    added validation to add and edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,14 @@
+const { validationResult } = require("express-validator");
+
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
+        hasError: false,
         editing: false,
+        errorMessage: null,
     });
 };
 
@@ -14,6 +18,21 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
     const userId = req.user;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+
+        return res.status(422).render("admin/edit-product", {
+            pageTitle: "Add Product",
+            path: "/admin/edit-product",
+            editing: false,
+            hasError: true,
+            product: { title, imageUrl, price, description },
+            errorMessage: errors.array()[0].msg,
+        });
+    }
 
     const product = new Product({
         title,
@@ -47,7 +66,9 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: "Edit Product",
                 path: "/admin/edit-product",
                 editing: editMode,
+                hasError: false,
                 product: product,
+                errorMessage: null,
             });
         })
         .catch((err) => console.log(err));
@@ -59,6 +80,27 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        // console.log(errors);
+
+        return res.status(422).render("admin/edit-product", {
+            pageTitle: "Edit Product",
+            path: "/admin/edit-product",
+            editing: true,
+            hasError: true,
+            product: {
+                title: updatedTitle,
+                imageUrl: updatedImageUrl,
+                price: updatedPrice,
+                description: updatedDescription,
+                _id: prodId,
+            },
+            errorMessage: errors.array()[0].msg,
+        });
+    }
 
     Product.findById(prodId)
         .then((product) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,5 @@
-const path = require("path");
-
 const express = require("express");
+const { body } = require("express-validator");
 
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
@@ -13,11 +12,37 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 
 router.get("/products", isAuth, adminController.getProducts);
 
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+    "/add-product",
+    [
+        body("title")
+            .isString()
+            .withMessage("Please enter a valid Title")
+            .trim(),
+        body("imageUrl").isURL().withMessage("Please enter a valid Image Url"),
+        body("price").isFloat().withMessage("Please enter a valid Price"),
+        body("description").trim(),
+    ],
+    isAuth,
+    adminController.postAddProduct
+);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+    "/edit-product",
+    [
+        body("title")
+            .isString()
+            .withMessage("Please enter a valid Title")
+            .trim(),
+        body("imageUrl").isURL().withMessage("Please enter a valid Image Url"),
+        body("price").isFloat().withMessage("Please enter a valid Price"),
+        body("description").trim(),
+    ],
+    isAuth,
+    adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
