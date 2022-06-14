b18ebb9fb0b8c14bf9c6ae62699510dd4de0dd1f
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-06-15 00:21:14 +0530

    implemented payments using stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,9 @@ const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
+const Stripe = require("stripe")(
+    "sk_test_51L3I7ySBCsEKV2GdOBgQU6999BDdJN2BqUm8HLdArTKL5Gat9hmYus4tpxEiwZNuksGH9WfgZlTSDvZ54VtTEkde006UE1nwMz"
+);
 
 const ITEMS_PER_PAGE = 3;
 
@@ -133,7 +136,59 @@ exports.postCartDeleteProduct = (req, res, next) => {
         });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+    let products;
+    let total = 0;
+    req.user
+        .populate("cart.items.productId")
+        .then((user) => {
+            products = user.cart.items;
+            total = 0;
+
+            products.forEach((p) => {
+                total += p.quantity * p.productId.price;
+            });
+
+            return Stripe.checkout.sessions.create({
+                payment_method_types: ["card"],
+                line_items: products.map((p) => {
+                    return {
+                        name: p.productId.title,
+                        description: p.productId.description,
+                        amount: p.productId.price * 100,
+                        currency: "inr",
+                        quantity: p.quantity,
+                    };
+                }),
+                success_url:
+                    req.protocol +
+                    "://" +
+                    req.get("host") +
+                    "/checkout/success",
+                cancel_url:
+                    req.protocol +
+                    "://" +
+                    req.get("host") +
+                    "/checkout/cancel",
+            });
+        })
+        .then((session) => {
+            res.render("shop/checkout", {
+                path: "/checkout",
+                pageTitle: "Checkout",
+                products: products,
+                totalSum: total,
+                sessionId: session.id,
+            });
+        })
+        .catch((err) => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
     req.user
         .populate("cart.items.productId")
         .then((user) => {
